394800c46adc719060586530fb7f9ae4a488cf04
Sultana Kausher <49339354+sultanakausher@users.noreply.github.com>
2021-01-06 19:47:50 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,7 +2,6 @@
  * Data-Structures-in-Java
  * BinaryTree.java
  */
-package com.deepak.data.structures.Tree;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,7 +9,6 @@ import java.util.Queue;
 /**
  * Binary Tree implementation
  * 
- * @author Deepak
  *
  * @param <E>
  */
